865925713d24184d3ba69def80517e2b5c149b60
Joseph Meltzer <u6060661@anu.edu.au>
2017-07-29 10:18:33 +1000

    Initial commit

---
+++ app/src/main/java/au/com/govhack/velocity/velocity/MainMenuActivity.java
@@ -0,0 +1,19 @@
+package au.com.govhack.velocity.velocity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.google.maps.GeoApiContext;
+
+public class MainMenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_menu);
+    }
+
+    public static void main(String[] args) {
+
+    }
+}

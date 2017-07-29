01f4a0ba1579bd4714bf2db5b7339ea74b3d3595
Joseph Meltzer <u6060661@anu.edu.au>
2017-07-29 14:27:26 +1000

    Map with polyline (a random one) and marker

--- app/src/main/java/au/com/govhack/velocity/velocity/MainMenuActivity.java
+++ app/src/main/java/au/com/govhack/velocity/velocity/MainMenuActivity.java
@@ -1,5 +1,9 @@
 package au.com.govhack.velocity.velocity;
 
+import android.content.pm.PackageManager;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,9 +13,13 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.Polyline;
+import com.google.android.gms.maps.model.PolylineOptions;
 
 
 public class MainMenuActivity extends AppCompatActivity implements OnMapReadyCallback {
+
+    boolean mLocationPermissionGranted;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,5 +46,17 @@ public class MainMenuActivity extends AppCompatActivity implements OnMapReadyCal
                 .title("Marker in Sydney"));
         googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
 
+        Polyline polyline1 = googleMap.addPolyline(new PolylineOptions()
+                .add(
+                        new LatLng(-35.016, 143.322),
+                        new LatLng(-34.747, 145.592),
+                        new LatLng(-34.364, 147.891),
+                        new LatLng(-33.501, 150.217),
+                        new LatLng(-32.306, 149.248),
+                        new LatLng(-32.491, 147.309)));
+        polyline1.setColor(0xFF0060C0);
+
     }
+
+
 }

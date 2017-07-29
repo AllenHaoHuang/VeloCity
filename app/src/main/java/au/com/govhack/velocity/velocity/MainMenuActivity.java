cc88dbf9a17ca7166ef82cc415e6efdc876d23b0
Thomas William Hamer <u6081614>
2017-07-29 18:11:51 +1000

    Merge branch 'workingBranch' of https://github.com/josephdmeltzer/VeloCity
    # Conflicts:
    #	app/src/main/java/au/com/govhack/velocity/velocity/MainMenuActivity.java

--- app/src/main/java/au/com/govhack/velocity/velocity/MainMenuActivity.java
+++ app/src/main/java/au/com/govhack/velocity/velocity/MainMenuActivity.java
@@ -132,7 +132,7 @@ public class MainMenuActivity extends AppCompatActivity implements OnMapReadyCal
         @Override
         public void run() {
             try  {
-                getDataFromUrl("http://10.0.2.2:1234/getRoute?origin=canberra&destination="+destination+"%20Canberra&option=Fastest");
+                raw_route = getDataFromUrl("http://10.0.2.2:1234/getRoute?origin="+origin+"&destination="+destination+"&option=Fastest");
             } catch (Exception e) {
                 e.printStackTrace();
             }
@@ -189,7 +189,6 @@ public class MainMenuActivity extends AppCompatActivity implements OnMapReadyCal
         if ((event.getAction() == KeyEvent.ACTION_DOWN) && (keyCode == KeyEvent.KEYCODE_ENTER))
         {
 
-
             destination = edittext.getText().toString();
 
             getRoute.start();
@@ -200,9 +199,12 @@ public class MainMenuActivity extends AppCompatActivity implements OnMapReadyCal
                 e.printStackTrace();
             }
 
-            System.out.println(raw_route);
+            DirectionsObject directions = JSON.parse(raw_route, DirectionsObject.class);
+            System.out.println(directions.getOverviewPolyline().getPoints());
 
-
+            Polyline routeTo = gmap.addPolyline(new PolylineOptions()
+                    .addAll(Decoder.decode(directions.getOverviewPolyline().getPoints())));
+            routeTo.setColor(0xFF0060C0);
 
 
 

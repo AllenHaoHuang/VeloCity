4df4a76c74483f05c7745ad9d84536229faf8c4c
Thomas William Hamer <u6081614>
2017-07-29 14:35:55 +1000

    Where to? bar added

--- app/src/main/java/au/com/govhack/velocity/velocity/MainMenuActivity.java
+++ app/src/main/java/au/com/govhack/velocity/velocity/MainMenuActivity.java
@@ -1,7 +1,16 @@
 package au.com.govhack.velocity.velocity;
 
+import android.content.Context;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -10,20 +19,45 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import static au.com.govhack.velocity.velocity.R.id.textView;
 
-public class MainMenuActivity extends AppCompatActivity implements OnMapReadyCallback {
+
+public class MainMenuActivity extends AppCompatActivity implements OnMapReadyCallback, View.OnKeyListener {
+
+    EditText edittext;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
 
+        EditText edittextproductnumber = (EditText) findViewById(R.id.editText);
+        edittextproductnumber.setOnKeyListener(this);
+
         // Get the SupportMapFragment and request notification
         // when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+        this.edittext = (EditText) findViewById(R.id.editText);
     }
+
+
+    @Override
+    public boolean onKey(View v, int keyCode, KeyEvent event) {
+        // TODO Auto-generated method stub
+
+        // Listen to "Enter" key press
+        if ((event.getAction() == KeyEvent.ACTION_DOWN) && (keyCode == KeyEvent.KEYCODE_ENTER))
+        {
+            System.out.println(edittext.getText());
+            return true;
+        }
+
+        return false;
+
+    }
+
 
     public static void main(String[] args) {
 
